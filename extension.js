344e0e7b63508f601f5a46eb13863938b1a1c9f2
jonian <jonian@hardpixel.eu>
2022-08-23 18:33:38 +0300

    remove extra spacing from search entry

--- extension.js
+++ extension.js
@@ -121,6 +121,7 @@ class ClipboardIndicator extends PanelMenu.Button {
     });
 
     const entryItem = new PopupMenu.PopupBaseMenuItem({
+      style_class: 'ci-history-search-section',
       reactive: false,
       can_focus: false,
     });
