SUPERCILEX/gnome-clipboard-history

commit 8c4fff69275cd05243e87439d07af943964b9dae

Fix for GNOME 46 (#161)

extension.js
@@ -84,7 +84,7 @@ class ClipboardIndicator extends PanelMenu.Button {
     });
     hbox.add_child(this._buttonText);
     this._downArrow = PopupMenu.arrowIcon(St.Side.BOTTOM);
-    hbox.add(this._downArrow);
+    hbox.add_child(this._downArrow);
     this.add_child(hbox);
 
     this._fetchSettings();
@@ -125,7 +125,7 @@ class ClipboardIndicator extends PanelMenu.Button {
       reactive: false,
       can_focus: false,
     });
-    entryItem.add(this.searchEntry);
+    entryItem.add_child(this.searchEntry);
     this.menu.addMenuItem(entryItem);
 
     this.menu.connect('open-state-changed', (self, open) => {
@@ -153,9 +153,9 @@ class ClipboardIndicator extends PanelMenu.Button {
       style_class: 'ci-history-menu-section',
       overlay_scrollbars: true,
     });
-    favoritesScrollView.add_actor(this.favoritesSection.actor);
+    favoritesScrollView.add_child(this.favoritesSection.actor);
 
-    this.scrollViewFavoritesMenuSection.actor.add_actor(favoritesScrollView);
+    this.scrollViewFavoritesMenuSection.actor.add_child(favoritesScrollView);
     this.menu.addMenuItem(this.scrollViewFavoritesMenuSection);
     this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
@@ -167,9 +167,9 @@ class ClipboardIndicator extends PanelMenu.Button {
       style_class: 'ci-history-menu-section',
       overlay_scrollbars: true,
     });
-    this.historyScrollView.add_actor(this.historySection.actor);
+    this.historyScrollView.add_child(this.historySection.actor);
 
-    this.scrollViewMenuSection.actor.add_actor(this.historyScrollView);
+    this.scrollViewMenuSection.actor.add_child(this.historyScrollView);
 
     this.menu.addMenuItem(this.scrollViewMenuSection);
 
@@ -181,7 +181,7 @@ class ClipboardIndicator extends PanelMenu.Button {
       vertical: false,
     });
 
-    actionsSection.actor.add(actionsBox);
+    actionsSection.actor.add_child(actionsBox);
     this.menu.addMenuItem(actionsSection);
 
     const prevPage = new PopupMenu.PopupBaseMenuItem();
@@ -192,7 +192,7 @@ class ClipboardIndicator extends PanelMenu.Button {
       }),
     );
     prevPage.connect('activate', this._navigatePrevPage.bind(this));
-    actionsBox.add(prevPage);
+    actionsBox.add_child(prevPage);
 
     const nextPage = new PopupMenu.PopupBaseMenuItem();
     nextPage.add_child(
@@ -202,9 +202,9 @@ class ClipboardIndicator extends PanelMenu.Button {
       }),
     );
     nextPage.connect('activate', this._navigateNextPage.bind(this));
-    actionsBox.add(nextPage);
+    actionsBox.add_child(nextPage);
 
-    actionsBox.add(new St.BoxLayout({ x_expand: true }));
+    actionsBox.add_child(new St.BoxLayout({ x_expand: true }));
 
     this.privateModeMenuItem = new PopupMenu.PopupSwitchMenuItem(
       _('Private mode'),
@@ -217,7 +217,7 @@ class ClipboardIndicator extends PanelMenu.Button {
         this.privateModeMenuItem.state,
       );
     });
-    actionsBox.add(this.privateModeMenuItem);
+    actionsBox.add_child(this.privateModeMenuItem);
     this._updatePrivateModeState();
 
     const clearMenuItem = new PopupMenu.PopupBaseMenuItem();
@@ -227,7 +227,7 @@ class ClipboardIndicator extends PanelMenu.Button {
         style_class: 'popup-menu-icon',
       }),
     );
-    actionsBox.add(clearMenuItem);
+    actionsBox.add_child(clearMenuItem);
 
     const settingsMenuItem = new PopupMenu.PopupBaseMenuItem();
     settingsMenuItem.add_child(
@@ -237,7 +237,7 @@ class ClipboardIndicator extends PanelMenu.Button {
       }),
     );
     settingsMenuItem.connect('activate', this._openSettings.bind(this));
-    actionsBox.add(settingsMenuItem);
+    actionsBox.add_child(settingsMenuItem);
 
     if (ENABLE_KEYBINDING) {
       this._bindShortcuts();
@@ -1021,7 +1021,7 @@ class ClipboardIndicator extends PanelMenu.Button {
     this._notifSource.connect('destroy', () => {
       this._notifSource = undefined;
     });
-    Main.messageTray.add(this._notifSource);
+    Main.messageTray.add_child(this._notifSource);
   }
 
   _showNotification(title, message, transformFn) {

commit 19c6d922446ed38f2799ed1a6facd0776c680dbf

Fix notifications on GNOME 46
 * MessageTray.{Source,Notification} constructor take objects
 * 8c4fff69275cd05243e87439d07af943964b9dae mistakenly changed
   Main.messageTray.add() to Main.messageTray.add_child()
 * MessageTray.Source.showNotification() renamed to MessageTray.Source.addNotification()
 * MessageTray.Notifcation.setTransient() removed
 * MessageTray.Notification.update() removed

extension.js
@@ -1014,14 +1014,14 @@ class ClipboardIndicator extends PanelMenu.Button {
       return;
     }
 
-    this._notifSource = new MessageTray.Source(
-      this.extension.uuid,
-      INDICATOR_ICON,
-    );
+    this._notifSource = new MessageTray.Source({
+      title: this.extension.indicatorName,
+      iconName: INDICATOR_ICON,
+    });
     this._notifSource.connect('destroy', () => {
       this._notifSource = undefined;
     });
-    Main.messageTray.add_child(this._notifSource);
+    Main.messageTray.add(this._notifSource);
   }
 
   _showNotification(title, message, transformFn) {
@@ -1037,22 +1037,26 @@ class ClipboardIndicator extends PanelMenu.Button {
 
     let notification;
     if (this._notifSource.count === 0) {
-      notification = new MessageTray.Notification(
-        this._notifSource,
+      notification = new MessageTray.Notification({
+        source: this._notifSource,
         title,
-        message,
-      );
+        body: message,
+        isTransient: true,
+      });
     } else {
       notification = this._notifSource.notifications[0];
-      notification.update(title, message, { clear: true });
+      notification.set({
+        title,
+        body: message,
+      });
+      notification.clearActions();
     }
 
     if (typeof transformFn === 'function') {
       transformFn(notification);
     }
 
-    notification.setTransient(true);
-    this._notifSource.showNotification(notification);
+    this._notifSource.addNotification(notification);
   }
 
   _updatePrivateModeState() {